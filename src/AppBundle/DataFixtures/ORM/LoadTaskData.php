b02b50f4b444b5059acfa35167a904152cd9b58b
tuyetrinhvo <you@example.com>
2018-02-24 21:10:44 +0100

    Edit views list task and loadTaskData

--- src/AppBundle/DataFixtures/ORM/LoadTaskData.php
+++ src/AppBundle/DataFixtures/ORM/LoadTaskData.php
@@ -14,27 +14,32 @@ class LoadTaskData extends AbstractFixture implements FixtureInterface, OrderedF
     public function load(ObjectManager $manager)
     {
         $task1 = new Task();
-        $task1->setTitle("Une tâche sans auteur");
-        $task1->setContent("Tâche ajoutée par datafixtures");
+        $task1->setTitle("Tâche crée par auteur anonyme");
+        $task1->setContent("Ancienne tâche ajoutée par auteur anonyme");
 
         $task2 = new Task();
-        $task2->setTitle("Deuxième tâche sans auteur");
-        $task2->setContent("Deuxième tâche ajoutée par datafixtures");
+        $task2->setTitle("Tâche crée par auteur anonyme");
+        $task2->setContent("Deuxième ancienne tâche ajoutée par auteur anonyme");
 
         $task3 = new Task();
-        $task3->setTitle("Tâche crée par auteur User");
-        $task3->setContent("Troisième tâche ajoutée par datafixtures");
-        $task3->setAuthor($this->getReference('myuser'));
+        $task3->setTitle("Tâche crée par auteur anonyme");
+        $task3->setContent("Troisième ancienne tâche ajoutée par auteur anonyme");
 
         $task4 = new Task();
         $task4->setTitle("Tâche crée par auteur Admin");
-        $task4->setContent("Quatrième tâche ajoutée par datafixtures");
+        $task4->setContent("Quatrième tâche ajoutée par auteur Admin");
         $task4->setAuthor($this->getReference('myadmin'));
+
+        $task5 = new Task();
+        $task5->setTitle("Tâche crée par auteur User");
+        $task5->setContent("Cinquièmre tâche ajoutée par auteur User");
+        $task5->setAuthor($this->getReference('myuser'));
 
         $manager->persist($task1);
         $manager->persist($task2);
         $manager->persist($task3);
         $manager->persist($task4);
+        $manager->persist($task5);
         $manager->flush();
     }
 
