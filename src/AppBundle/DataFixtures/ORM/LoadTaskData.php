da0ca18f75aa4041ad2d5befc893a37cdcf80f2c
tuyetrinhvo <you@example.com>
2018-02-24 18:12:30 +0100

    Edit datafixtures classes

--- src/AppBundle/DataFixtures/ORM/LoadTaskData.php
+++ src/AppBundle/DataFixtures/ORM/LoadTaskData.php
@@ -2,27 +2,38 @@
 
 namespace AppBundle\DataFixtures\ORM;
 
-
 use AppBundle\Entity\Task;
+use Doctrine\Common\DataFixtures\AbstractFixture;
 use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 
 
-class LoadTaskData implements FixtureInterface
+class LoadTaskData extends AbstractFixture implements FixtureInterface, OrderedFixtureInterface
 {
     public function load(ObjectManager $manager)
     {
         $task1 = new Task();
-        $task1->setTitle("Test d'une tâche !");
-        $task1->setContent("Ajout d'une tâche grace aux data-fixtures.");
+        $task1->setTitle("Une tâche sans auteur");
+        $task1->setContent("Tâche ajoutée par datafixtures");
 
         $task2 = new Task();
-        $task2->setTitle("Test d'une autre tâche");
-        $task2->setContent("Ajout d'une seconde tâche !");
+        $task2->setTitle("Deuxième tâche sans auteur");
+        $task2->setContent("Deuxième tâche ajoutée par datafixtures");
+
+        $task3 = new Task();
+        $task3->setTitle("Tâche crée par auteur User");
+        $task3->setContent("Troisième tâche ajoutée par datafixtures");
+        $task3->setAuthor($this->getReference('myuser'));
 
         $manager->persist($task1);
         $manager->persist($task2);
+        $manager->persist($task3);
         $manager->flush();
     }
 
+    public function getOrder()
+    {
+        return 4;
+    }
 }
--- src/AppBundle/DataFixtures/ORM/LoadUserData.php
+++ src/AppBundle/DataFixtures/ORM/LoadUserData.php
@@ -3,14 +3,11 @@
 namespace AppBundle\DataFixtures\ORM;
 
 use AppBundle\Entity\User;
-use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\AbstractFixture;
-use Doctrine\Common\DataFixtures\FixtureInterface;
 use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
-use Symfony\Component\Security\Core\Encoder\PasswordEncoderInterface;
 
 
 class LoadUserData extends AbstractFixture implements OrderedFixtureInterface,ContainerAwareInterface
@@ -27,31 +24,24 @@ class LoadUserData extends AbstractFixture implements OrderedFixtureInterface,Co
 
     public function load(ObjectManager $manager)
     {
-        $user1 = new User();
-        $user1->setUsername('user');
-        $user1->setEmail('user@gmail.com');
+        $password = $this->container->get('security.password_encoder');
 
-        $password = 'aqw';
-        $encoder = $this->container->get('security.password_encoder');
-        $encodePass = $encoder->encodePassword($user1, $password);
+        $user = new User();
+        $user->setUsername('user');
+        $user->setEmail('user@gmail.com');
+        $user->setPassword($password->encodePassword($user, 'aqw'));
+        $user->setRoles(['ROLE_USER']);
 
-        $user1->setPassword($encodePass);
-        $user1->setRoles(['ROLE_USER']);
+        $admin = new User();
+        $admin->setUsername('admin');
+        $admin->setEmail('admin@gmail.com');
+        $admin->setPassword($password->encodePassword($admin, 'zsx'));
+        $admin->setRoles(['ROLE_ADMIN']);
 
-        $user2 = new User();
-        $user2->setUsername('admin');
-        $user2->setEmail('admin@gmail.com');
-
-        $password2 = 'zsx';
-        $encoder2 = $this->container->get('security.password_encoder');
-        $encodePass2 = $encoder2->encodePassword($user2, $password2);
-
-        $user2->setPassword($encodePass2);
-        $user2->setRoles(['ROLE_ADMIN']);
-
-        $manager->persist($user1);
-        $manager->persist($user2);
+        $manager->persist($user);
+        $manager->persist($admin);
         $manager->flush();
+        $this->addReference('myuser', $user);
     }
 
     public function getOrder()
