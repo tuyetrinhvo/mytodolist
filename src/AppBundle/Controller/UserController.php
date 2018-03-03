e4bf6b2dd6503853f21991567858c69f8358c14d
tuyetrinhvo <you@example.com>
2018-03-03 11:56:28 +0100

    edit UserController and security.yml

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -8,11 +8,13 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 
 class UserController extends Controller
 {
     /**
      * @Route("/users", name="user_list")
+     * @Security("has_role('ROLE_ADMIN')")
      * @Method({"GET"})
      */
     public function listAction()
@@ -22,6 +24,7 @@ class UserController extends Controller
 
     /**
      * @Route("/users/create", name="user_create")
+     * @Security("has_role('ROLE_ADMIN')")
      * @Method({"GET", "POST"})
      */
     public function createAction(Request $request)
@@ -49,6 +52,7 @@ class UserController extends Controller
 
     /**
      * @Route("/users/{id}/edit", name="user_edit")
+     * @Security("has_role('ROLE_ADMIN')")
      * @Method({"GET", "POST"})
      */
     public function editAction(User $user, Request $request)
--- src/AppBundle/Form/Type/UserType.php
+++ src/AppBundle/Form/Type/UserType.php
@@ -18,6 +18,7 @@ class UserType extends AbstractType
             ->add('username', TextType::class, ['label' => "Nom d'utilisateur"])
             ->add('password', RepeatedType::class, [
                 'type'              => PasswordType::class,
+                'invalid_message'   => 'Les deux mots de passe doivent correspondre.',
                 'required'          => true,
                 'first_options'     => ['label' => 'Mot de passe'],
                 'second_options'    => ['label' => 'Tapez le mot de passe à nouveau'],
