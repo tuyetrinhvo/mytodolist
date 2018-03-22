8cf58afd1d7e9107559044685d52349af632c505
tuyetrinhvo <you@example.com>
2018-03-22 16:27:39 +0100

    Edit code style some files with Php_codesniffer

--- src/AppBundle/Command/EditOldTasksCommand.php
+++ src/AppBundle/Command/EditOldTasksCommand.php
@@ -1,10 +1,16 @@
 <?php
 
 /**
- * Created by PhpStorm.
- * User: trinhvo
+ * Class Doc Comment
+ *
+ * PHP version 7.0
+ *
+ * @category PHP_Class
+ * @package  AppBundle
+ * @author   trinhvo <ttvdep@gmail.com>
+ * @license  License Name
+ * @link     Link Name
  */
-
 namespace AppBundle\Command;
 
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
@@ -12,8 +18,22 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use AppBundle\Entity\User;
 
+/**
+ * Class EditOldTasksCommand
+ *
+ * @category PHP_Class
+ * @package  AppBundle\Command
+ * @author   trinhvo <ttvdep@gmail.com>
+ * @license  License Name
+ * @link     Link Name
+ */
 class EditOldTasksCommand extends ContainerAwareCommand
 {
+    /**
+     * Function configure
+     *
+     * @return void
+     */
     protected function configure()
     {
         $this
@@ -21,10 +41,20 @@ class EditOldTasksCommand extends ContainerAwareCommand
             ->setDescription('Update old tasks : link them to an anonymous user');
     }
 
+    /**
+     * Function execute
+     *
+     * @param InputInterface  $input  Some argument description
+     * @param OutputInterface $output Some argument description
+     *
+     * @return int|null|void
+     */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $entityManager = $this->getContainer()->get('doctrine.orm.entity_manager');
-        $oldTasks = $entityManager->getRepository('AppBundle:Task')->findByAuthor(null);
+        $oldTasks = $entityManager->getRepository(
+            'AppBundle:Task'
+        )->findByAuthor(null);
 
         if (!empty($oldTasks)) {
             $userAnonyme = new User();
