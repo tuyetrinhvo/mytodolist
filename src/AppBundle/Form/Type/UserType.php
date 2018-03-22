f599d0c5e2f08a73c0d80b7614734f3e57ce1fce
tuyetrinhvo <you@example.com>
2018-03-22 15:32:49 +0100

    Edit code style FormType with Php_codesniffer

--- src/AppBundle/Form/Type/UserType.php
+++ src/AppBundle/Form/Type/UserType.php
@@ -1,5 +1,15 @@
 <?php
-
+/**
+ * Class Doc Comment
+ *
+ * PHP version 7.0
+ *
+ * @category PHP_Class
+ * @package  AppBundle
+ * @author   trinhvo <ttvdep@gmail.com>
+ * @license  License Name
+ * @link     Link Name
+ */
 namespace AppBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
@@ -10,8 +20,25 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 
+/**
+ * Class UserType
+ *
+ * @category PHP_Class
+ * @package  AppBundle\Form\Type
+ * @author   trinhvo <ttvdep@gmail.com>
+ * @license  License Name
+ * @link     Link Name
+ */
 class UserType extends AbstractType
 {
+    /**
+     * Function buildForm User
+     *
+     * @param FormBuilderInterface $builder Some argument description
+     * @param array                $options Some argument description
+     *
+     * @return void
+     */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
@@ -21,10 +48,12 @@ class UserType extends AbstractType
                 RepeatedType::class,
                 [
                 'type'              => PasswordType::class,
-                'invalid_message'   => 'Les deux mots de passe doivent correspondre.',
+                'invalid_message'   =>
+                    'Les deux mots de passe doivent correspondre.',
                 'required'          => true,
                 'first_options'     => ['label' => 'Mot de passe'],
-                'second_options'    => ['label' => 'Tapez le mot de passe à nouveau'],
+                'second_options'    => [
+                    'label' => 'Tapez le mot de passe à nouveau'],
                 ]
             )
             ->add('email', EmailType::class, ['label' => 'Adresse email'])
